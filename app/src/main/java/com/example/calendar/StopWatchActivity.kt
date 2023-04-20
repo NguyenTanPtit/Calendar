b650ff7b79e6714074a79e56a3cf0256fd3828a2
Nguyen Tan <riptan2001@gmail.com>
2023-04-20 19:22:16 +0700

    update stopWatch

--- app/src/main/java/com/example/calendar/StopWatchActivity.kt
+++ app/src/main/java/com/example/calendar/StopWatchActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calendar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -71,7 +72,9 @@ class StopWatchActivity : AppCompatActivity() {
 
     private fun setOnClick(){
         back.setOnClickListener{
-            onBackPressedDispatcher.onBackPressed()
+            val i = Intent(this, HomeActivity::class.java)
+            i.putExtra("fragment", "lit")
+            setResult(RESULT_OK, i)
             finish()
         }
 
